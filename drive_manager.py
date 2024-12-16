704f15b937ad27aceccca41ee416fd28b1067ada
Adrien Jaussaud <adrien.jaussaud@gmail.com>
2024-12-16 12:50:47 +0100

    invalid import

--- drive_manager.py
+++ drive_manager.py
@@ -1,7 +1,7 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
 from google.auth.transport.requests import Request
 
 import os
