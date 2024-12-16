a072ea17648288e49cca6a2eccd78a9d2d021a60
Adrien Jaussaud <adrien.jaussaud@gmail.com>
2024-12-16 17:16:17 +0100

    test simple

--- test.py
+++ test.py
@@ -16,29 +16,24 @@ class TestDhManagerGPIO(unittest.TestCase):
         """Clean up GPIO resources after each test."""
         GPIO.cleanup()
 
-    def test_control_fan_on(self):
-        """Test turning the fan ON."""
-        self.dh.control_fan(True)
-        self.assertTrue(self.dh.fan_on)
-        self.assertEqual(GPIO.input(self.dh.fan_port), GPIO.HIGH)
+    def test_control_fan(self):
+        while True:
+            temperature = int(input('Enter temperature: '))
+            """Test turning the fan ON."""
+            if(temperature > 25):
+                self.dh.control_fan(True)
+            else:
+                self.dh.control_fan(False)
 
-    def test_control_fan_off(self):
-        """Test turning the fan OFF."""
-        self.dh.control_fan(False)
-        self.assertFalse(self.dh.fan_on)
-        self.assertEqual(GPIO.input(self.dh.fan_port), GPIO.LOW)
+    # def test_control_humidifier(self):
+    #     while True:
+    #         humidity = int(input('Enter humidity: '))
+    #         """Test turning the fan ON."""
+    #         if(humidity > 50):
+    #             self.dh.control_humidifier(True)
+    #         else:
+    #             self.dh.control_humidifier(False)
 
-    def test_control_humidifier_on(self):
-        """Test turning the humidifier ON."""
-        self.dh.control_humidifier(True)
-        self.assertTrue(self.dh.humidifier_on)
-        self.assertEqual(GPIO.input(self.dh.humid_port), GPIO.HIGH)
-
-    def test_control_humidifier_off(self):
-        """Test turning the humidifier OFF."""
-        self.dh.control_humidifier(False)
-        self.assertFalse(self.dh.humidifier_on)
-        self.assertEqual(GPIO.input(self.dh.humid_port), GPIO.LOW)
 
 if __name__ == "__main__":
     unittest.main()
